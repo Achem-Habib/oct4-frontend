825beb3354b35785ef6ab5b7e8246417912c880a
Achem Habib <achemnomaer57@gmail.com>
2024-04-01 17:41:10 +0600

    resolved the image loading issue in product detail page

--- components/product/ProductImages.js
+++ components/product/ProductImages.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { normalizeImageUrl } from "@/lib";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -8,9 +9,9 @@ const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 export default function ProductImages({ main_image, other_images }) {
   const mainImage = [
     {
-      original: `${backendUrl}${main_image}`,
+      original: normalizeImageUrl(main_image),
       originalAlt: "product image",
-      thumbnail: `${backendUrl}${main_image}`,
+      thumbnail: normalizeImageUrl(main_image),
       thumbnailAlt: "product image",
       thumbnailHeight: 40,
       thumbnailWidth: 40,
@@ -18,9 +19,9 @@ export default function ProductImages({ main_image, other_images }) {
   ];
 
   const otherImages = other_images.map((item) => ({
-    original: `${backendUrl}${item.image}`,
+    original: normalizeImageUrl(item.image),
     originalAlt: item.description || "product image",
-    thumbnail: `${backendUrl}${item.image}`,
+    thumbnail: normalizeImageUrl(item.image),
     thumbnailAlt: item.description || "product image",
     thumbnailHeight: 40,
     thumbnailWidth: 40,
--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+      },
+      {
         protocol: "https",
         hostname: "oct4-admin.onrender.com",
         port: "",
